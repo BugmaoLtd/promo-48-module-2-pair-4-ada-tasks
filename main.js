2442a924a076b81f3b8b5a46ac1d3cb8af12b343
AndreaChust <andrea.chust@hotmail.com>
2025-03-10 10:41:53 +0100

    tasks

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ const inputTask = document.querySelector(".js-newTaskInput");
 const addBtn = document.querySelector(".js-taskBtn");
 const list = document.querySelector(".js-task");
 
+
 const handleClick = (event) => {
   event.preventDefault();
+  const newTaskInputValue = inputTask.value;
 };
 
 addBtn.addEventListener("click", handleClick);
@@ -24,8 +26,20 @@ const tasks = [
 /* Seleccionar el input de nueva tarea el boton de agregar y el ul de la lista
 cuando la usuaria hace click en Agregar
 recogemos el valor del input
-lo pintamos en la lista*/
+lo pintamos en la lista
+Si el checkbox esta marcado: la tarea se tacha
+Si completed === true /añade la clase through
+
+*/
 
 for (const task of tasks) {
-  // pintar la tarea en la lista
+    list.innerHTML += `<li class="tachado"><input type="checkbox" id=""> ${task.name}</li>`;
+}
+
+const tachado = document.querySelector(".tachado");
+
+
+if (tasks.completed === true) {
+    tachado.classList.add("through"); 
+    console.log(through); 
 }
